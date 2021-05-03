3224a1bcae83801e609ce3c2b02575cd22be5667
da <valbendan@hotmail.com>
2021-05-03 22:22:02 +0800

    知乎重定向

--- us.js
+++ us.js
@@ -1,12 +1,13 @@
 // ==UserScript==
-// @name        暴力猴脚本
+// @name        da的暴力猴脚本
 // @namespace   ViolentMonkey Scripts
 // @match       *://tieba.baidu.com/*
 // @match       *://*.csdn.net/*
+// @match       *://*.zhihu.com/*
 // @grant       none
 // @version     1.0
 // @author      da
-// @description 百度贴吧登录屏蔽
+// @description 百度贴吧登录屏蔽, CSDN 登录屏蔽
 // ==/UserScript==
 
 
@@ -37,7 +38,35 @@
         }, interval);
     }
 
+    /// 知乎重新定向
+    function zhihuAutoRedirect() {
+        if (location.host === 'link.zhihu.com') {
+            let regRet = location.search.match(/target=(.+?)(&|$)/);
+            if (regRet && regRet.length === 3) {
+                location.href = decodeURIComponent(regRet[1]);
+            }
+            return;
+        }
+
+        window.addEventListener('click', function (e) {
+            let dom       = e.target,
+                max_times = 5;
+            while (dom && max_times--) {
+                if (dom.nodeName.toUpperCase() === 'A') {
+                    let regRet = dom.search.match(/target=(.+?)(&|$)/);
+                    if (regRet && regRet.length === 3) {
+                        dom.href = decodeURIComponent(regRet[1]);
+                    }
+                    return;
+                } else {
+                    dom = dom.parentNode;
+                }
+            }
+        });
+    }
+
     baiduBBSHideLogin();
     csdnHideLogin();
+    zhihuAutoRedirect();
 })();
 
