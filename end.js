a65fc397714dabc5a1a1d4db5d5d5dcd82bfbe7f
da <valbendan@hotmail.com>
2021-11-23 11:55:16 +0800

    fix download url

--- end.js
+++ end.js
@@ -10,7 +10,7 @@
 // @grant       none
 // @version     1.0
 // @author      da
-// @downloadURL https://cdn.jsdelivr.net/gh/valbendan/MonkeyKing@main/us.js
+// @downloadURL https://cdn.jsdelivr.net/gh/valbendan/MonkeyKing@main/end.js
 // @supportURL  https://github.com/valbendan/MonkeyKing/issues
 // @homepageURL https://github.com/valbendan/MonkeyKing
 // @description 百度贴吧登录屏蔽, CSDN 登录屏蔽, 知乎自动重定向，开源中国，维基百科自动重定向
